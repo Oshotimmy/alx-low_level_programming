315528e3a1f34203a2453ed23ad4eee16c6a28af
Oshotimmy <oshodife2010@gmail.com>
2022-09-11 17:22:29 -0700

    Task 8 fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,18 @@
-#include <unistd.h>
+#include<stdio.h>
+
 /**
- *  * main - Entry
+ *  * main - Entry Point
  *   *
- *    * Return: 1
+ *    * Description: prints to the stderr, "and that piece of art
+ *     * is useful\" - Dora Korpar, 2015-10-19"
+ *      *
+ *       * Return: Always 0 (Success)
  */
 int main(void)
 {
-		char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+		fprintf(
+					stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+						);
 
-			write(2, str1, 59);
-				return (1);
+			return (1);
 }
