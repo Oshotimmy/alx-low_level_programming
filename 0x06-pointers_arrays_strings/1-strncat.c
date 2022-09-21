06220d071346071e92fc89a93475e463e0c9d334
Oshotimmy <oshodife2010@gmail.com>
2022-09-21 15:27:52 -0700

    strncat fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,14 +9,14 @@
  * Return: returns new concatenated string
  */
 
-char *_strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	 int index = 0, dest_len = 0;
 
 	while (dest[index++])
 		dest_len++;
 
-	for (index = 0; src[index] && < n; index++)
+	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
 
 	return (dest);
