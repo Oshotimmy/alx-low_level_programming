46b84d20376d8e53e5f050eebe2d37cbebc7e0b7
Oshotimmy <oshodife2010@gmail.com>
2022-09-21 15:10:11 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: string to be appended upon
+ * @src: string to be completed when desk end
+ * @n: integer parameter to complete index
+ *
+ * Return: returns new concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n);
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && < n; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
+
