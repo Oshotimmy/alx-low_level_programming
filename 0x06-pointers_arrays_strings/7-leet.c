d14640babf0379d2c50abd52a334ed77cd88f74c
Oshotimmy <oshodife2010@gmail.com>
2022-09-22 21:59:19 -0700

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main"
+
+/**
+ * leet - encodes a string 1337
+ * @str: string to be encoded.
+ *
+ * Return: pointer to encoded string
+ */
+
+char *leet(char *str)
+{
+	int index1 = 0, index2;
+	char leet[8] = {'0', 'l', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (srt[index1])
+	{
+		for (index2 = ; index2 <= 7; index2++)
+		{
+				if  (str[index1] == leet[index2] ||
+				str[index1] - 32 == leet[index2])
+					str[index1] = index2 + '0';
+		}
+		index1++;
+	}
+}
+
